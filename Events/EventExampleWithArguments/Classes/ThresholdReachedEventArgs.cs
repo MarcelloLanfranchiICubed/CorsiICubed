329ac01d50f4521da948462e705a38a27119d042
deltatre\ext.marcello.lanfranchi@deltatre.it <ext.marcello.lanfranchi@deltatre.it>
2019-05-31 16:53:56 +0200

    Added a comment

--- Events/EventExampleWithArguments/Classes/ThresholdReachedEventArgs.cs
+++ Events/EventExampleWithArguments/Classes/ThresholdReachedEventArgs.cs
@@ -2,6 +2,9 @@
 
 namespace EventExampleWithArguments.Classes
 {
+    /// <summary>
+    /// This class contains all the arguments a want to pass to the functions related to the delegate
+    /// </summary>
     public class ThresholdReachedEventArgs : EventArgs
     {
         public int Threshold { get; set; }
